f4c211d866bbece7a3d49e8ac3d3bf7a1a318a31
LF <94565149+Trision@users.noreply.github.com>
2024-03-27 16:57:45 -0300

    Update main.py

--- DescontosMain/main.py
+++ DescontosMain/main.py
@@ -3,7 +3,7 @@ import math
 import mysql
 
 #declarando página
-st.image('logo.png', 100)
+st.image('DescontosMain/logo.png', 100)
 st.title('Cálcular desconto')
 carteira = st.radio('Carteira', ['Lendico', 'Consig', 'Help'], horizontal = True)
 valor = st.number_input('Digite o valor em aberto: ')
